c73931a1de43b95d2c1e26628e54cacebe362d1a
Anuj Kumar Sharma <anuj@titan.local>
2025-12-19 23:46:33 +0530

    Part 2, VectorStore and Embeddings added

--- src/test/java/com/codingshuttle/learn_spring_ai/service/AIServiceTests.java
+++ src/test/java/com/codingshuttle/learn_spring_ai/service/AIServiceTests.java
@@ -15,4 +15,27 @@ public class AIServiceTests {
         var joke = aiService.getJoke("Cats");
         System.out.println(joke);
     }
+
+    @Test
+    public void testEmbedText() {
+        var embed = aiService.getEmbedding("This is a big text here");
+        System.out.println(embed.length);
+        for(float e: embed) {
+            System.out.print(e+" ");
+        }
+    }
+
+    @Test
+    public void testStoreData() {
+        aiService.ingestDataToVectorStore();
+    }
+
+    @Test
+    public void testSimilaritySearch() {
+        var res = aiService.similaritySearch("movies of different genre");
+        for(var doc: res) {
+            System.out.println(doc);
+        }
+
+    }
 }
